f780b6c394cb7d1cf434977f34bce3e19953ef07
raeoyw <mozzerae@uw.edu>
2020-08-19 00:39:12 -0400

    I just push what I have done andI think I am at a.6

--- problem-a/src/App.js
+++ problem-a/src/App.js
@@ -1,4 +1,5 @@
-import React, { Component } from 'react'; //import React Component
+import React, { Component, useState } from 'react'; //import React Component
+import { render } from 'enzyme';
 
 const EXAMPLE_SENATORS = [  
   { id: 'C000127',  name: 'Maria Cantwell', state: 'WA',  party: 'Democrat', phone: '202-224-3441', twitter: 'SenatorCantwell' },
@@ -6,3 +7,68 @@ const EXAMPLE_SENATORS = [
 ];
 
 /* Your code goes here */
+export class App extends Component {
+  render() {
+    return (
+      <div className="container">
+        <h1>US Senators 2019</h1>
+        <SenatorTable />
+      </div>
+    )
+  }
+}
+
+export class SenatorTable extends Component {
+  render() {
+    return (
+      <table className="table table-bordered">
+        <TableHeader cols={["Name", "State", "Phone", "Twitter"]} />
+
+        <SenatorRow senator={EXAMPLE_SENATORS} />
+
+      </table>
+    )
+  }
+}
+
+export class TableHeader extends Component {
+  render() {
+    let colsArray = this.props.cols.map((colNameString) => {
+      let component = <th key={colNameString}>{colNameString}</th>;
+      return component;
+    })
+
+    return (
+      <thead>
+        <tr>
+          {colsArray}
+        </tr>
+      </thead>
+    )
+  }
+}
+
+export class SenatorRow extends Component {
+  render() {
+    let senatorRow = this.props.senator.map((senatorName) => {
+      return ([
+        <tr>
+          <td key={senatorName}>{senatorName.name}</td>
+          <td key={senatorName}>{senatorName.party.charAt(0)} - {senatorName.state}</td>
+          <td key={senatorName}>
+            <a href="tel:">{senatorName.phone}</a>
+          </td>
+          <td key={senatorName}>
+          <a href="https://twitter.com/{senatorName.twitter}">@{senatorName.twitter}</a>
+          </td>
+        </tr>
+      ])
+    });
+
+    return (
+      <tbody>
+        {senatorRow}
+      </tbody>
+    )
+  }
+}
--- problem-a/src/index.js
+++ problem-a/src/index.js
@@ -1,4 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import senatorData from './senators.json'
 
 //render the App component here!
+import { App } from './App';
+
+ReactDOM.render(<App />, document.getElementById('root'));
